af626f1fdf47c18fe9e66ba9d1ce441636898459
EliWalles <eli.v.walles@gmail.com>
2016-02-24 14:16:10 +0500

    Advanta

--- advanta/tpl/js/script.js
+++ advanta/tpl/js/script.js
@@ -1,39 +1,19 @@
 $(function () {
+	
 	var x=0;
-	if($( "header:in-viewport" ).text()) {
-		$('body .img-hold:eq(0)').show();
-		$('body .img-hold:eq(1)').hide();
-		$('body .img-hold:eq(2)').hide();
-		$('body .img-hold:eq(3)').hide();
-	}
-	if($( "#inf:in-viewport" ).text()) {
-		$('body .img-hold:eq(0)').hide();
-		$('body .img-hold:eq(1)').show();
-		$('body .img-hold:eq(2)').hide();
-		$('body .img-hold:eq(3)').hide();
-	}
-	if($( "#num:in-viewport" ).text()) {
-		$('body .img-hold:eq(0)').hide();
-		$('body .img-hold:eq(1)').hide();
-		$('body .img-hold:eq(2)').show();
-		$('body .img-hold:eq(3)').hide();
-	}
-	if($( "#why:in-viewport" ).text()) {
-		$('body .img-hold:eq(0)').hide();
-		$('body .img-hold:eq(1)').hide();
-		$('body .img-hold:eq(2)').hide();
-		$('body .img-hold:eq(3)').show();
-	}
-	if($( "#why:in-viewport" ).text()) {
-		$('body .img-hold:eq(0)').hide();
-		$('body .img-hold:eq(1)').hide();
-		$('body .img-hold:eq(2)').hide();
-		$('body .img-hold:eq(3)').show();
-	}
-	if($( "#res:in-viewport(-300)" ).text()) {
-		if (!x) $('.circlestat').circliful();x=1;
-	}
+	ParalaxImg();
 	$(window).scroll(function(){
+		ParalaxImg();
+	});
+	var wW = $(window).width()/2+120;
+	var wWm = $(window).width()/2-120-683;
+	$('#all .block1').css('background-position',wW+'px center');
+	$('#all .block2').css('background-position',wWm+'px center');
+	$('#all .block3').css('background-position',wW+'px center');
+	
+});
+
+function ParalaxImg() {
 	if($( "header:in-viewport" ).text()) {
 		$('body .img-hold:eq(0)').show();
 		$('body .img-hold:eq(1)').hide();
@@ -67,5 +47,4 @@ $(function () {
 	if($( "#res:in-viewport(-300)" ).text()) {
 		if (!x) $('.circlestat').circliful();x=1;
 	}
-	})
-});
+}
