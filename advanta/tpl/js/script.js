4be3f3c06dda9e15904d59a5738801990b6b08cf
Eli <eli.v.walles@gmail.com>
2016-04-04 10:38:06 +0600

    Update script.js

--- advanta/tpl/js/script.js
+++ advanta/tpl/js/script.js
@@ -4,7 +4,7 @@ jQuery(document).ready(function(){
 	grayscale.prepare( jQuery('#dov .table .table-cell img') );
 	grayscale( jQuery('#dov .table .table-cell img') );
 	
-  	jQuery('#block-new5').appendTo( jQuery('#block-new4') );
+  	jQuery('#block-new5').appendTo(jQuery('#fFormBlock'));
 	
 	jQuery('a[href="#form"]').click(function(e){
 		jQuery('html, body').animate({scrollTop:jQuery(document).height()}, 'slow');
